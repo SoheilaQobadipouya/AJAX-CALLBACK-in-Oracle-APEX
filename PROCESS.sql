8596aed305e0bebb8719a3055ed26573860d6997
Soheila QobadiPouya <64842866+SoheilaQobadipouya@users.noreply.github.com>
2023-12-09 11:01:10 +0330

    Update PROCESS.sql

--- PROCESS.sql
+++ PROCESS.sql
@@ -4,7 +4,7 @@ DECLARE
 BEGIN
 PARAM1:=apex_application.g_x01;
 PARAM2:=apex_application.g_x02;
-htp.p('You have called the Ajax Process with to parameters ' || PARAM1 ||' and '|| PARAM2 ||' and page item value '||:P3_PARAM );
+htp.p('You have called the Ajax Process with to parameters ' || PARAM1 ||' and '|| PARAM2 ||' and page item value '||:P1_PARAM );
 EXCEPTION
 WHEN OTHERS THEN    
     htp.p('ERROR IN AJAX CALLBACK PROCESS');
